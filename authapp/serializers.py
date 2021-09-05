0931fa69805ac209eb642abf19d4ceaaea9b2cf3
Abinash karki <karkiabinash777@gmail.com>
2021-09-05 14:38:36 +0545

    making email field required

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from . models import *
+
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,)
     class Meta: 
         model = User
         fields = ['username', 'email', 'password']
